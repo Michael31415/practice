2332c099e4d5300407950ac8951de33759d9755c
Michael31415 <mike@mbox.in.ua>
2016-12-11 10:20:45 +0200

    Copied parts from shatokhin to better comply with given task

--- CreateSpline.m
+++ CreateSpline.m
@@ -1,4 +1,4 @@
-function [interpolationSpline] = CreateSpline(points, func)
+function [interpolationSpline, splineFunction] = CreateSpline(points, func, leftCondition)
 	if strcmp(class(func), 'function_handle')
 		values = arrayfun(func, points);
 	elseif length(func) == length(points)
@@ -6,10 +6,52 @@ function [interpolationSpline] = CreateSpline(points, func)
 	else
 		error('Unknown format of input argument func.');
 	end;
+	if isrow(points)
+		points = points';
+	end;
 	if isrow(values)
 		values = values';
 	end;
-	matrix = CreateSEMatrix(points, values);
+	matrix = CreateSEMatrix(points, values, leftCondition);
 	solution = SolveSE(matrix);
 	interpolationSpline = FormSpline(points, values, solution);
+	splineFunction = @(derivative, t)(EvaluateSpline(points, interpolationSpline, derivative, t));
+end;
+
+function result = EvaluateSpline(points, interpolationSpline, derivative, t)
+	[row, relativeValue] = SelectRow(points, interpolationSpline, t);
+	if row == 0
+		result = 0;
+		return;
+	end;
+	coefficients = EvaluateCoefficients(length(row), derivative);
+	powers = relativeValue .^ (length(row) - derivative - 1 : -1 : 0);
+	result = sum(row(1 : length(powers)) .* powers .* coefficients(1 : length(powers)));
+end;
+
+function coefficients = EvaluateCoefficients(rowLength, derivative)
+	if derivative == 0
+		coefficients = ones(1, rowLength);
+		return;
+	end;
+	coefficients = prod((ones(derivative, 1) * (rowLength - 1 : -1 : 0)) - ((0 : derivative - 1)' * ones(1, rowLength)), 1);
+end;
+
+function [row, relativeValue] = SelectRow(points, interpolationSpline, t)
+	if t < points(1)
+		row = 1;
+		relativeValue = 0;
+		return;
+	end;
+	if t >= points(end)
+		row = interpolationSpline(end, :);
+		relativeValue = t - points(end - 1);
+		return;
+	end;
+
+	points = t - points;
+	interpolationSpline = interpolationSpline(points >= 0, :);
+	row = interpolationSpline(end, :);
+	points = points(points >= 0);
+	relativeValue = points(end);
 end;
--- main.m
+++ main.m
@@ -3,67 +3,29 @@ function [] = main(func, points, plotPoints, leftCondition)
 		func = @(t)(sin(t^2));
 	end;
 	if ~exist('points')
-		points = 0 : 1 : 5;
+		points = sqrt(0 : 0.05 : 1) * 5;
 	end;
 	if ~exist('plotPoints')
-		plotPoints = 0 : 0.01 : 5;
+		plotPoints = 0 : 0.001 : 5;
 	end;
 	if ~exist('leftCondition')
 		leftCondition = 0;
 	end;
 
-	interpolationSpline = CreateSpline(points, func, leftCondition);
-	splineVal = @(t)(EvaluateSpline(points, interpolationSpline, 0, t));
-	splineDerivative = @(t)(EvaluateSpline(points, interpolationSpline, 1, t));
-	splineSecondDerivative = @(t)(EvaluateSpline(points, interpolationSpline, 2, t));
+	[interpolationSpline, splineFunc] = CreateSpline(points, func, leftCondition);
+	splineVal = @(t)(splineFunc(0, t));
+	splineDerivative = @(t)(splineFunc(1, t));
+	splineSecondDerivative = @(t)(splineFunc(2, t));
 
 	figure('units','normalized','outerposition',[0 0 1 1], 'paperorientation', 'landscape');
-	plot(plotPoints, arrayfun(func, plotPoints), 'k-.', plotPoints, arrayfun(splineVal, plotPoints), 'k', points, arrayfun(func, points), 'kx');
-	legend('interpolated function', 'interpolation spline', 'pivot points');
+	plot(plotPoints, arrayfun(func, plotPoints), 'k--', plotPoints, arrayfun(splineVal, plotPoints), 'k', points, arrayfun(func, points), 'kx');
+	legend('interpolated function', 'interpolation spline', 'pivot points', 'location', 'southoutside');
 	title(sprintf('Maximal deviation: %e', max(abs(arrayfun(func, plotPoints) - arrayfun(splineVal, plotPoints)))));
 	grid minor;
 	print -dpdf ./result.pdf;
 	figure('units','normalized','outerposition',[0 0 1 1], 'paperorientation', 'landscape');
 	plot(plotPoints, arrayfun(splineDerivative, plotPoints), 'k--', plotPoints, arrayfun(splineSecondDerivative, plotPoints), 'k');
-	legend('spline first derivative', 'spline second derivative');
+	legend('spline first derivative', 'spline second derivative', 'location', 'southoutside');
 	grid minor;
 	print -dpdf -append ./result.pdf;
-end;
-
-function result = EvaluateSpline(points, interpolationSpline, derivative, t)
-	[row, relativeValue] = SelectRow(points, interpolationSpline, t);
-	if row == 0
-		result = 0;
-		return;
-	end;
-	coefficients = EvaluateCoefficients(length(row), derivative);
-	powers = relativeValue .^ (length(row) - derivative - 1 : -1 : 0);
-	result = sum(row(1 : length(powers)) .* powers .* coefficients(1 : length(powers)));
-end;
-
-function coefficients = EvaluateCoefficients(rowLength, derivative)
-	if derivative == 0
-		coefficients = ones(1, rowLength);
-		return;
-	end;
-	coefficients = prod((ones(derivative, 1) * (rowLength - 1 : -1 : 0)) - ((0 : derivative - 1)' * ones(1, rowLength)), 1);
-end;
-
-function [row, relativeValue] = SelectRow(points, interpolationSpline, t)
-	if t < points(1)
-		row = 1;
-		relativeValue = 0;
-		return;
-	end;
-	if t >= points(end)
-		row = interpolationSpline(end, :);
-		relativeValue = t - points(end - 1);
-		return;
-	end;
-
-	points = t - points;
-	interpolationSpline = interpolationSpline(points >= 0, :);
-	row = interpolationSpline(end, :);
-	points = points(points >= 0);
-	relativeValue = points(end);
 end;
