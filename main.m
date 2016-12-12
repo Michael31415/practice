56a797ddcab44f9380ae6718e0a167e3bed82217
Michael31415 <mike@mbox.in.ua>
2016-12-12 07:29:23 +0200

    Untracked pdf and minor fixes

--- main.m
+++ main.m
@@ -1,4 +1,4 @@
-function [] = main(func, points, plotPoints, leftCondition)
+function [] = main(func, points, plotPoints, condition)
 	if ~exist('func')
 		func = @(t)(sin(t^2));
 	end;
@@ -8,19 +8,24 @@ function [] = main(func, points, plotPoints, leftCondition)
 	if ~exist('plotPoints')
 		plotPoints = 0 : 0.001 : 5;
 	end;
-	if ~exist('leftCondition')
-		leftCondition = 0;
+	if ~exist('condition')
+		condition = 0;
 	end;
 
-	[interpolationSpline, splineFunc] = CreateSpline(points, func, leftCondition);
+	[interpolationSpline, splineFunc] = CreateSpline(points, func, condition);
 	splineVal = @(t)(splineFunc(0, t));
 	splineDerivative = @(t)(splineFunc(1, t));
 	splineSecondDerivative = @(t)(splineFunc(2, t));
 
 	figure('units','normalized','outerposition',[0 0 1 1], 'paperorientation', 'landscape');
-	plot(plotPoints, arrayfun(func, plotPoints), 'k--', plotPoints, arrayfun(splineVal, plotPoints), 'k', points, arrayfun(func, points), 'kx');
-	legend('interpolated function', 'interpolation spline', 'pivot points', 'location', 'southoutside');
-	title(sprintf('Maximal deviation: %e', max(abs(arrayfun(func, plotPoints) - arrayfun(splineVal, plotPoints)))));
+	if strcmp(class(func), 'function_handle')
+		plot(plotPoints, arrayfun(func, plotPoints), 'k--', plotPoints, arrayfun(splineVal, plotPoints), 'k', points, arrayfun(func, points), 'kx');
+		legend('interpolated function', 'interpolation spline', 'pivot points', 'location', 'southoutside');
+	else
+		plot(plotPoints, arrayfun(splineVal, plotPoints), 'k', points, arrayfun(func, points), 'kx');
+		legend('interpolation spline', 'pivot points', 'location', 'southoutside');
+	end;
+	title(sprintf('Maximal deviation: %e', max(abs(arrayfun(func, points) - arrayfun(splineVal, points)))));
 	grid minor;
 	print -dpdf ./result.pdf;
 	figure('units','normalized','outerposition',[0 0 1 1], 'paperorientation', 'landscape');
